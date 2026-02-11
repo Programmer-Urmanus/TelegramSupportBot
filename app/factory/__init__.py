b75b8790c550fe2404e2157cbe3090c2b6f96838
Programmer-Urmanus <1fufrik1@gmail.com>
2026-02-11 20:59:57 +0500

    feat(database): add SQLAlchemy session pool factory

--- app/factory/__init__.py
+++ app/factory/__init__.py
@@ -1,3 +1,4 @@
 from .app_config import create_app_config
+from .session_pool import create_session_pool
 
-__all__: list[str] = ["create_app_config"]
+__all__: list[str] = ["create_app_config", "create_session_pool"]
