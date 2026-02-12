57882f6aac4cfd068686a69695be590177896064
Programmer-Urmanus <1fufrik1@gmail.com>
2026-02-12 22:09:16 +0500

    feat(domain): add concrete repositories

--- app/services/database/repositories/__init__.py
+++ app/services/database/repositories/__init__.py
@@ -1,4 +1,11 @@
 from .base import BaseRepository
 from .general import Repository
+from .users import UsersRepository
+from .topics import TopicsRepository
 
-__all__: list[str] = ["BaseRepository", "Repository"]
+__all__: list[str] = [
+    "BaseRepository",
+    "Repository",
+    "UsersRepository",
+    "TopicsRepository",
+]
--- app/services/database/repositories/general.py
+++ app/services/database/repositories/general.py
@@ -1,13 +1,21 @@
 from __future__ import annotations
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from .base import BaseRepository
+from .users import UsersRepository
+from .topics import TopicsRepository
 
 
-class Repository:
+class Repository(BaseRepository):
     """
     Base repository abstraction.
     Provides access to SQLAlchemt session.
     """
 
+    users: UsersRepository
+    topics: TopicsRepository
+
     def __init__(self, session: AsyncSession) -> None:
         super().__init__(session=session)
+        self.users = UsersRepository(session=session)
+        self.topics = TopicsRepository(session=session)
